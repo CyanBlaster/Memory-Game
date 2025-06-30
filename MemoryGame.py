93519309ed2770c7a9a6a86d5e096c90c70ba552
CyanBlaster <michael.ng.do@gmail.com>
2025-06-29 22:20:11 -0400

    Made a working selector.

--- MemoryGame.py
+++ MemoryGame.py
@@ -36,18 +36,28 @@ def main():
         print(map)
         for y in range(len(map)):
              for x in range(len(map[y])):
-                pygame.draw.rect(screen, (255, 0, 0), (x * cellHeight - 1, y * cellWidth - 1, cellHeight, cellWidth), 1, border_radius = 1)
+                pygame.draw.rect(screen, (255, 0, 0), (x * cellHeight, y * cellWidth, cellHeight, cellWidth), 1, border_radius = 1)
                 
-                pygame.draw.rect(screen, (255, 0, 0), (x * cellHeight - 1, y * cellWidth - 1, cellHeight, cellWidth), 1, border_radius = 1)
+                pygame.draw.rect(screen, (255, 0, 0), (x * cellHeight, y * cellWidth, cellHeight, cellWidth), 1, border_radius = 1)
                 text_surface, rect = GAME_FONT.render(str(map[x][y]), (255, 0, 0))
                 screen.blit(text_surface, (x * cellHeight + 22.5, y * cellWidth + 5))
+        
+        pygame.draw.rect(screen, (255, 255, 255), (xIdx * cellHeight, yIdx * cellWidth, cellHeight, cellWidth), 1, border_radius = 5)
 
         for events in pygame.event.get():
                 if events.type == pygame.QUIT:
                     running = False
                 if events.type == pygame.KEYDOWN:
-                     if events.key == pygame.K_SPACE:
-                          print("x")
+                    if events.key == pygame.K_SPACE:
+                        print("x")
+                    elif events.key == pygame.K_LEFT:
+                        xIdx -= 1
+                    elif events.key == pygame.K_RIGHT:
+                        xIdx += 1
+                    elif events.key == pygame.K_UP:
+                        yIdx -= 1
+                    elif events.key == pygame.K_DOWN:
+                        yIdx += 1
 
 
 main()
